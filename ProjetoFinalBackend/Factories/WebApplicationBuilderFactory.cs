0e465392b79ea517b606dea87250bcdf9dbf2003
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2024-09-01 10:08:06 -0300

    Update WebApplicationBuilderFactory.cs

--- ProjetoFinalBackend/Factories/WebApplicationBuilderFactory.cs
+++ ProjetoFinalBackend/Factories/WebApplicationBuilderFactory.cs
@@ -1,10 +1,14 @@
-﻿namespace ProjetoFinalBackend.Api.Factories;
+﻿using ProjetoFinalBackend.Infra.CrossCutting.Providers;
+
+namespace ProjetoFinalBackend.Api.Factories;
 
 public static class WebApplicationBuilderFactory
 {
     public static WebApplicationBuilder CreateApp(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var configuration = CustomConfigurationProvider.GetConfiguration(builder.Environment.EnvironmentName);
+        builder.Configuration.AddConfiguration(configuration);
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
