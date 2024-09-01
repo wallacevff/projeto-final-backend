7d7e02122abe9ec7c402ce0d1804901775532986
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2024-09-01 10:07:50 -0300

    Update IoCManager.cs

--- ProjetoFinalBackend.IoC/IoCManager.cs
+++ ProjetoFinalBackend.IoC/IoCManager.cs
@@ -15,21 +15,47 @@ public static class IoCManager
     #region "Private Methods"
     private static IServiceCollection AddAllServicesByTypes(this IServiceCollection services, Type typeInterface, Type implementationType)
     {
-        IEnumerable<Type> repositoryInterfaces = typeInterface.Assembly
-            .GetTypes()
-            .Where(i => i.IsInterface && i != typeInterface);
+        IEnumerable<Type> projectInterfaces = GetDescendentInterfaces(typeInterface);
+        return AddServiceOfAllTypesAndDescendentTypes
+            (services, projectInterfaces, implementationType);
+        
+    }
 
-        IEnumerable<Type> implementedTypes;
-
-        foreach (Type type in repositoryInterfaces)
+    private static IServiceCollection AddServiceOfAllTypesAndDescendentTypes(IServiceCollection services, IEnumerable<Type> interfaceTypes, Type implementationType)
+    {
+        foreach (var implementedTye in interfaceTypes)
         {
-            implementedTypes = implementationType.Assembly.GetTypes().Where(t => !t.IsInterface && !t.IsAbstract && t.IsAssignableTo(type));
-            foreach (Type implementedType in implementedTypes)
-            {
-                services.AddTransient(type, implementedType);
-            }
+            var implementedTypes = GetImplementedTypesFromInterface(implementedTye, implementationType);
+            AddServiceScoped(services, implementedTye, implementedTypes);
         }
         return services;
     }
+
+    private static  IEnumerable<Type> GetDescendentInterfaces(Type typeInterface)
+    {
+        return typeInterface.Assembly
+            .GetTypes()
+            .Where(i => i.IsInterface && i != typeInterface);
+    }
+
+    private static IEnumerable<Type> GetImplementedTypesFromInterface
+        (Type projectInterface, Type implementationType)
+    {
+        return implementationType.Assembly.GetTypes()
+                .Where(t => !t.IsInterface && !t.IsAbstract && t.IsAssignableTo(projectInterface));
+    }
+
+    private static IServiceCollection AddServiceScoped(
+        IServiceCollection services,
+        Type interfaceType,
+        IEnumerable<Type> implementedTypes)
+    {
+        foreach (Type implementedType in implementedTypes)
+        {
+            services.AddScoped(interfaceType, implementedType);
+        }
+        return services;
+    }
+
     #endregion
 }
