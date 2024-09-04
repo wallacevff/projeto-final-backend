2953885c113477c8a4d5a08204ad20fe7bfed49a
Wallace Fortuna <wallace.fortuna@ses.rj.gov.br>
2024-09-04 18:03:26 -0300

    Create RespostaAluno.cs

---
+++ ProjetoFinalBackend.Domain/ConteudoModels/RespostaAluno.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoFinalBackend.Domain.SistemaModels.ArquivoModels;
+
+namespace ProjetoFinalBackend.Domain.ConteudoModels;
+
+public class RespostaAluno
+{
+    public Guid CursoId { get; set; }
+    public Guid TurmaId { get; set; }
+    public Guid AtividadeId { get; set; }
+    public Guid AlunoId { get; set; }
+    public Guid RespostaId { get; set; }
+    public IList<Arquivo>? Anexo { get; set; }
+    public string? Resposta { get; set; }
+}
